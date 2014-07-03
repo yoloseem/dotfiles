5d8e8d3468f453bbab6de50b328a80f17a82024f
Hyunjun Kim <yoloseem@gmail.com>
2014-07-03 12:46:18 +0900

    Python installing script

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,19 @@
-easy_install SpoofMAC
+#!/bin/sh
+#
+# Python
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Python.
+
+# Check for pip
+if test ! $(which pip)
+then
+  echo "  Installing pip for you."
+  sudo easy_install pip
+fi
+
+# Install virtualenv & wrapper
+sudo pip install virtualenvwrapper
+sudo pip install Pygments
+
+exit 0
